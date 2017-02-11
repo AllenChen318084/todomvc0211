3f0bea242f07eb329a5043269e7485887d935549
Will 保哥 <doggy.huang@gmail.com>
2017-02-11 14:18:51 +0800

    任務 11：套用 @Output() 裝飾器 (Decorator)

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import {Input, Component,  OnInit} from '@angular/core';
+import {Input, Output, EventEmitter, Component,  OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -9,6 +9,8 @@ export class FooterComponent implements OnInit {
 
   @Input() todos: any[];
 
+  @Output() clearCompleted = new EventEmitter<any>();
+
   constructor() { }
 
   ngOnInit() {
@@ -16,5 +18,9 @@ export class FooterComponent implements OnInit {
 
   get active_items() {
     return this.todos.filter(v => !v.done).length;
+  }
+
+  btnClear() {
+    this.clearCompleted.emit();
   }
 }
